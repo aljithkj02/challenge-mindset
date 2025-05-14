929ebe11a28d6b57a99c04dddc26ebd5cf4b5951
Aljith K J <kwdeathlok@gmail.com>
2025-05-14 15:46:13 +0530

    Update heading navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
     return (
         <nav className="bg-black/80 backdrop-blur-md fixed top-0 w-full z-50 px-8 py-5 flex justify-between items-center shadow-lg">
-            <h1 className="text-3xl font-bold tracking-tight text-white">Challenge Mindset Building</h1>
+            <h1 className="text-2xl md:text-3xl font-bold tracking-tight text-white">Challenge Mindset Building</h1>
         </nav>
     );
 }
