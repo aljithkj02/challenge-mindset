29573f1d9ef5bc04a304a435d44db76fb177a4ad
Aljith K J <kwdeathlok@gmail.com>
2025-06-03 16:33:31 +0530

    change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ export const Hero = () => {
     };
 
     return (
-        <section className="relative bg-blue-950 text-white px-6 md:px-12 pt-28 text-center overflow-hidden">
+        <section className="relative  bg-black text-white px-6 md:px-12 pt-28 text-center overflow-hidden">
             <div className="relative z-10 max-w-4xl mx-auto">
                 <motion.div
                     initial={{ opacity: 0, y: 50 }}
@@ -26,7 +26,7 @@ export const Hero = () => {
                     viewport={{ once: true }}
                     className="bg-white/5 border border-white/10 backdrop-blur-md rounded-2xl px-8 py-10 shadow-lg"
                 >
-                    <h2 className="text-xl md:text-3xl font-medium italic leading-relaxed text-white tracking-wide">
+                    <h2 className="text-xl md:text-3xl font-medium italic leading-relaxed text-[#FFD700] tracking-wide">
                         എല്ലാരും പിന്നെയും നിന്നിൽ സംശയിക്കാൻ തുടങ്ങി. <br />
                         നീ ആരാണെന്ന് അവരെ ഓർമിപ്പിച്ചു കൊള്ളുക!
                     </h2>
